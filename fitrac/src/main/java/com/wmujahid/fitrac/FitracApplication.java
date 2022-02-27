058a2923a1f53d0b269eaeae2c59cd9fb29e5938
wmujahid <wmujahid47@gmail.com>
2022-02-27 15:41:23 -0500

    Added project skeleton built from spring initializr

---
+++ fitrac/src/main/java/com/wmujahid/fitrac/FitracApplication.java
@@ -0,0 +1,13 @@
+package com.wmujahid.fitrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitracApplication.class, args);
+	}
+
+}
---
+++ fitrac/src/test/java/com/wmujahid/fitrac/FitracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmujahid.fitrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
