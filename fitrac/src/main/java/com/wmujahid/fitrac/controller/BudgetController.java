e4aa75254621bfb26e2c566abb8e8e4d455e3f0f
wmujahid <wmujahid47@gmail.com>
2022-02-27 22:59:51 -0500

    finished crud items for budget

--- fitrac/src/main/java/com/wmujahid/fitrac/controller/BudgetController.java
+++ fitrac/src/main/java/com/wmujahid/fitrac/controller/BudgetController.java
@@ -4,11 +4,10 @@ import com.wmujahid.fitrac.model.Budget;
 import com.wmujahid.fitrac.service.BudgetService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/budget")
@@ -19,9 +18,33 @@ public class BudgetController {
         this.budgetService = budgetService;
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<Budget>> getAllBudgets() {
         List<Budget> budgets = budgetService.findAllBudgets();
         return new ResponseEntity<>(budgets, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Budget> getBudgetById(@PathVariable(value = "id") Long id) {
+            Budget budget = budgetService.findBudgetById(id);
+        return new ResponseEntity<>(budget, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Budget> createBudget(@RequestBody Budget budget) {
+        Budget newBudget = budgetService.addBudget(budget);
+        return new ResponseEntity<>(newBudget, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Budget> updateBudget(@RequestBody Budget budget){
+        Budget updateBudget = budgetService.updateBudget(budget);
+        return new ResponseEntity<>(updateBudget, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteBudget(@PathVariable(value = "id") Long id) {
+        budgetService.findBudgetById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
