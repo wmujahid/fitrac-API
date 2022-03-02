1216577a51c57565459a6741434fbc4146d825db
wmujahid <wmujahid47@gmail.com>
2022-03-02 05:36:43 -0500

    added income model

--- fitrac/src/main/java/com/wmujahid/fitrac/model/Income.java
+++ fitrac/src/main/java/com/wmujahid/fitrac/model/Income.java
@@ -1,4 +1,62 @@
 package com.wmujahid.fitrac.model;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Income {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column
+    private Number amount;
+
+    @Column
+    private String description;
+
+    public Income(Long id, Number amount, String description) {
+        this.id = id;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public Income() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Number getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Number amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Income{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
