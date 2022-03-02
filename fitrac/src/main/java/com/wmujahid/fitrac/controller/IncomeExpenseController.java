301b4f15149b58c3e4a8ede2149dad05168d84ef
wmujahid <wmujahid47@gmail.com>
2022-03-02 06:08:22 -0500

    added income/expense controller routes

--- fitrac/src/main/java/com/wmujahid/fitrac/controller/IncomeExpenseController.java
+++ fitrac/src/main/java/com/wmujahid/fitrac/controller/IncomeExpenseController.java
@@ -1,7 +1,10 @@
 package com.wmujahid.fitrac.controller;
 
 import com.wmujahid.fitrac.model.Budget;
+import com.wmujahid.fitrac.model.Expense;
+import com.wmujahid.fitrac.model.Income;
 import com.wmujahid.fitrac.service.BudgetService;
+import com.wmujahid.fitrac.service.IncomeExpenseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,45 +15,71 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/budget")
 public class IncomeExpenseController {
-    private final BudgetService budgetService;
+    private final IncomeExpenseService incomeExpenseService;
 
-    public IncomeExpenseController(BudgetService budgetService){
-        this.budgetService = budgetService;
+    public IncomeExpenseController(IncomeExpenseService incomeExpenseService){
+        this.incomeExpenseService = incomeExpenseService;
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<List<Budget>> getAllBudgets() {
-        List<Budget> budgets = budgetService.findAllBudgets();
-        return new ResponseEntity<>(budgets, HttpStatus.OK);
+    @GetMapping("/income/all")
+    public ResponseEntity<List<Income>> getAllIncome() {
+        List<Income> incomes = incomeExpenseService.findAllIncomes();
+        return new ResponseEntity<>(incomes, HttpStatus.OK);
     }
 
-    @GetMapping("/find/{id}")
-    public ResponseEntity<Budget> getBudgetById(@PathVariable(value = "id") Long id) {
-            Budget budget = budgetService.findBudgetById(id);
-        return new ResponseEntity<>(budget, HttpStatus.OK);
+    @GetMapping("/income/find/{id}")
+    public ResponseEntity<Income> getIncomeById(@PathVariable(value = "id") Long id) {
+            Income income = incomeExpenseService.findIncomeById(id);
+        return new ResponseEntity<>(income, HttpStatus.OK);
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Budget> createBudget(@RequestBody Budget budget) {
-        Budget newBudget = budgetService.createBudget(budget);
-        return new ResponseEntity<>(newBudget, HttpStatus.CREATED);
+    @PostMapping("/income/create")
+    public ResponseEntity<Income> createIncome(@RequestBody Income income) {
+        Income newIncome = incomeExpenseService.createIncome(income);
+        return new ResponseEntity<>(newIncome, HttpStatus.CREATED);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Budget> updateBudget(@PathVariable(
-            value = "id") Long id, @RequestBody Budget budgetObject){
-        Budget updateBudget = budgetService.updateBudget(id, budgetObject);
-        return new ResponseEntity<>(updateBudget, HttpStatus.OK);
+    @PutMapping("/income/update/{id}")
+    public ResponseEntity<Income> updateIncome(@PathVariable(
+            value = "id") Long id, @RequestBody Income incomeObject){
+        Income updateIncome = incomeExpenseService.updateIncome(id, incomeObject);
+        return new ResponseEntity<>(updateIncome, HttpStatus.OK);
     }
 
-//    @DeleteMapping("/delete/{id}")
-//    public ResponseEntity<?> deleteBudget(@PathVariable(value = "id") Long id) {
-//        budgetService.deleteBudget(id);
-//        return new ResponseEntity<>(HttpStatus.OK);
-//    }
+    @DeleteMapping("/income/delete/{id}")
+    public Optional<Income> deleteIncome(@PathVariable(value = "id") Long id) {
+        return incomeExpenseService.deleteIncome(id);
+    }
 
-    @DeleteMapping("/delete/{id}")
-    public Optional<Budget> deleteBudget(@PathVariable(value = "id") Long id) {
-        return budgetService.deleteBudget(id);
+    //--------------------------------------------------------------------//
+
+    @GetMapping("/expense/all")
+    public ResponseEntity<List<Expense>> getAllExpense() {
+        List<Expense> expenses = incomeExpenseService.findAllExpenses();
+        return new ResponseEntity<>(expenses, HttpStatus.OK);
+    }
+
+    @GetMapping("/expense/find/{id}")
+    public ResponseEntity<Expense> getExpenseById(@PathVariable(value = "id") Long id) {
+        Expense expense = incomeExpenseService.findExpenseById(id);
+        return new ResponseEntity<>(expense, HttpStatus.OK);
+    }
+
+    @PostMapping("/expense/create")
+    public ResponseEntity<Expense> createExpense(@RequestBody Expense expense) {
+        Expense newExpense = incomeExpenseService.createExpense(expense);
+        return new ResponseEntity<>(newExpense, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/expense/update/{id}")
+    public ResponseEntity<Expense> updateExpense(@PathVariable(
+            value = "id") Long id, @RequestBody Expense expenseObject){
+        Expense updateExpense = incomeExpenseService.updateExpense(id, expenseObject);
+        return new ResponseEntity<>(updateExpense, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/expense/delete/{id}")
+    public Optional<Expense> deleteExpense(@PathVariable(value = "id") Long id) {
+        return incomeExpenseService.deleteExpense(id);
     }
 }
